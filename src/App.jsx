903976298af68ff68e6e3c6dc35721f2befd7a29
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-11-01 21:47:54 +0530

    Styled Grids

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import Icon from './components/Icon/Icon'
 import './App.css'
 import Card from './components/card/Card'
+import Grid from './components/Grid/Grid'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Card />
+      <Grid numberOfCards={9}/>
     </>
   )
 }
